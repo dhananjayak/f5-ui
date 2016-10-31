86adee44fa5e3a66cd634882a5beda9d724bbf55
dhananjayak <dhananjayak@yahoo.com>
2016-10-31 17:18:51 +0530

    added new views to ui-event

--- js/ui-event.js
+++ js/ui-event.js
@@ -1,5 +1,4 @@
 
-
 function wowInit(){
   new WOW().init();
 }
@@ -7,14 +6,19 @@ function wowInit(){
 function showBookNow(){
   F5.UIViews.templateView($('#book-now-container'), function(){
     $('#book-now-popup').modal('show');
+
+    F5.UIViews.bookNowHeaderView($('#booknow-header-view'));
   });
 }
 
 function uiViewsInit(){
   var hoursView = F5.UIViews.hoursView($('#hours-view')),
-      datesView = F5.UIViews.datesView($('#dates-view'));
+      datesView = F5.UIViews.datesView($('#dates-view')),
+      packagesView = F5.UIViews.packageView($('#packages-view')),
+      slotsView = F5.UIViews.slotsView($('#slots-view'));
 
    datesView.addOnChange(hoursView.refresh);
+   packagesView.addOnChange(slotsView.refresh);
 }
 
 function uiTemplateViewsInit(){
@@ -24,139 +28,14 @@ function uiTemplateViewsInit(){
 function init() {
   wowInit();
 
-  defaultPackageSel();
-
   uiViewsInit();
 
   uiTemplateViewsInit();
 }
 
-function defaultPackageSel() {
-    var $radios = $('input:radio[name=package]');
-    if($radios.is(':checked') === false) {
-        $("input[name=package][value=" + 1+ "]").prop('checked', true);
-
-        //Display the proper slots
-        bedslots(1);
-
-    }
-}
-
-
-function loadDate(){
-
-	//alert ('break');
-		//Get the current date
-		var now     = new Date();
-		var today = dateToSring(now);
-
-		 var fiveDays = F5.UI.dates(5);
-
-
-		for(var i = 0;i < 5; i++){
-
-		$('#checkInDate').append( new Option(fiveDays.display,fiveDays.value) );
-
-		}
-
-		// $('#checkInDate').append( new Option(today,1) );
-//
-// 		var tomorrow = dateToSring(addDays(now,1));
-//
-// 		$("#checkInDate").append( new Option(tomorrow,2) );
-//
-// 		var thirdDay  = dateToSring(addDays(now,2));
-//
-// 		$("#checkInDate").append( new Option(thirdDay,3) );
-//
-// 		var fourthDay  = dateToSring(addDays(now,3));
-// 		$("#checkInDate").append( new Option(fourthDay,4) );
-//
-// 		var fiveDay = dateToSring(addDays(now,4));
-// 		$("#checkInDate").append( new Option(fiveDay,5) );
-
-		$("#checkInDate").selectedIndex = 0;
-
-	}
-
-function loadTime() {
 
 
 
-	//alert ('break');
-    var now     = new Date();
-
-
-
-    var  checkin = $("#checkInDate option:selected").val();
-
-   // alert(checkin);
-
-    //Old code
-    //var selDate = stringToDate(checkin,"dd-mm-yyyy","-");
-
-   //alert(selDate);
-    //Compare the both date if the selected date current date then show the time slot from the current time.
-
-    //Get the time slot
-
-    //var timeSlot = getCheckInTimeList();
-
-
-   // alert(timeSlot);
-
-   //Empty the previous data
-   $('#checkInTime').empty();
-
-    //Check the current day with the user selected date
-    if(now.getDate() === checkin.getDate()){
-
-    	//alert("date are same");
-    	//Get the current time slot of the same day
-    	//var curTimeList = getCurrentTimeSlot(timeSlot);
-
-    	//for(var i = 0; i < curTimeList.length; i++){
-		//var option = options[i];
-  		 //$('#checkInTime').append( new Option(curTimeList[i],i) );
-		//}
-
-		var selDate = F5.UI.hours(now.getDate());
-
-   		// alert("date are not same");
-    	for(var i = 0, l = selDate.length; i < l; i++){
-			//var option = options[i];
-  		 		$('#checkInTime').append( new Option(selDate.display,selDate.value) );
-			}
-
-	//alert(i);
-	$('#checkInTime').selectedIndex = 0;
-
-    }else{
-
-    //set the time from the beginning of the day
-
-    	var selDate = F5.UI.hours(checkin);
-
-   		// alert("date are not same");
-    	for(var i = 0, l = selDate.length; i < l; i++){
-			//var option = options[i];
-  		 		$('#checkInTime').append( new Option(selDate.display,selDate.value) );
-			}
-
-			 var pos = timeslot.indexOf("09:00");
-			 if(typeof pos != undefiend){
-				$('#checkInTime').selectedIndex = pos;
-				}else{
-				$('#checkInTime').selectedIndex = 0;
-				}
-	}
-
-
-
-
-
-  //   return dateTime;
-}
 
 
 $("#confirmForm").submit(function(e) {
@@ -174,115 +53,6 @@ function displayConfirmForm(){
 	$("#checkAvailability").hide();
 	$("#confirm").show();
 
-}
-
-
-$(document).on('click',"#package1",function()
-{
-//alert(1);
-
- if ($(this).is(':checked')) {
-      // put your code here and your alert
-      bedslots(1);
-   }
-
-
-});
-
-$(document).on('click',"#package2",function()
-{
-//alert(1);
-	 if ($(this).is(':checked')) {
-
-      bedslots(2);
-   }
-
-});
-
-
-$(document).on('click',"#package3",function()
-{
-if ($(this).is(':checked')) {
-	loungeslots(0);
-	}
-
-});
-
-
-$(document).on('click',"#package4",function()
-{
-//alert(1);
-	if ($(this).is(':checked')) {
-		loungeslots(1);
-	}
-
-});
-
-
-function bedslots(isBed){
-
-	$('#slothours').empty();
-
-	//alert(1);
-	dataval = ['4 Hrs'];
-	dataid =[4];
-	dataPrice=[800];
-
-	if(isBed === 2){
-	dataval = ['4 Hrs','6 Hrs'];
-	dataid =[4,6];
-	dataPrice=[700,950];
-	}
- for(i=0; i < dataval.length ; i++ )
-            {
-
-                //var destDropDownMenu = "<li class =\"funkyradio-success\"><input type=\"radio\" name=\"radio\" id="+ dataid[i]+" value="+ dataid[i] +"><label for="+dataid[i] +" class=\"radio\">"+ dataval[i]+"- INR "+dataPrice[i]+"</label></input></li>"
-                var destDropDownMenu = "<li class =\"funkyradio-success\"><input type=\"radio\" name=\"radio\" id="+ dataid[i]+" value="+ dataid[i] +"><label for="+dataid[i] +" class=\"radio\">"+ dataval[i]+"</label></input></li>"
-                $(destDropDownMenu).appendTo('#slothours');
-            }
-
-           // $('input[name=foo]').prop('checked', true);
-
-   // $("input[name=foo]").prop('checked', 0);
-
-   var $radios = $('input:radio[name=slothours]');
-
-        //var valId = dataid[0];
-        ///alert(valId);
-        //$("#0").checked = true;
-   		//$radios.filter(dataid[0]).prop('checked', true);
-
-   		$("input[name=radio][value=" + dataid[0] + "]").prop('checked', true);
-
-}
-
-function loungeslots(isSofa){
-
-	$('#slothours').empty();
-	dataval = ['2 Hrs','4 Hrs','6 Hrs'];
-	dataid =[2,4,6];
-	dataPrice=[400,700,900];
-	if(isSofa === 1){
-		dataPrice=[250,500,750];
-	}
-
-
- 	for(i=0; i < dataval.length ; i++ )
-            {
-                //var destDropDownMenu = "<li class =\"funkyradio-success\"><input type=\"radio\" name=\"radio\" id="+ dataid[i]+" value="+ dataid[i] +"><label for="+dataid[i] +" class=\"radio\">"+ dataval[i]+ "- INR "+dataPrice[i]+"</label></input></li>"
-                var destDropDownMenu = "<li class =\"funkyradio-success\"><input type=\"radio\" name=\"radio\" id="+ dataid[i]+" value="+ dataid[i] +"><label for="+dataid[i] +" class=\"radio\">"+ dataval[i]+"</label></input></li>"
-                $(destDropDownMenu).appendTo('#slothours');
-            }
-
-           // $("#0").checked = true;
-        //    $("#slothours").prop('selectedIndex', 0);
-
-        //$('#slothours').selectedIndex = 0;;
-       // var valId = "value="+dataid[0];
-         //$('input:radio[name="radio"]').selectedIndex = 0;
-         $("input[name=radio][value=" + dataid[0] + "]").prop('checked', true);
-
-   		//$radios.filter(valId).prop('checked', true);
 }
 
 function bookNowClicked(){
