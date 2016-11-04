8549902baad2faae870d272bc98c74c4ca875c82
dhananjayak <dhananjayak@yahoo.com>
2016-11-04 15:01:47 +0530

    submit form only after validation

--- js/f5/f5-ui-booknow.js
+++ js/f5/f5-ui-booknow.js
@@ -14,7 +14,22 @@
       console.log(bookingDetailRequest);
     }
 
+    function initialize(){
+      console.log('book now initialization');
+      
+      $('#confirm-booking-form').validator().on('submit', function (e) {
+        if (e.isDefaultPrevented()) {
+          console.log('default prevented...');
+          // handle the invalid form...
+        } else {
+          // everything looks good!
+          confirmBooking({name:'#inputName', email:'#inputEmail', phone:'#inputPhone'});
+        }
+      });
+    }
+
     global.F5 = global.F5 || {};
 
-    global.F5.UIViews.confirmBooking = confirmBooking;
+    //global.F5.UIViews.bookNow.confirmBooking = confirmBooking;
+    global.F5.UIViews.initializeBookNow = initialize;
 })(window);
